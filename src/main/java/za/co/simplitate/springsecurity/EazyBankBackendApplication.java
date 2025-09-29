f135739b39cd86f974816f404e5f2a2233a04616
Zwaks <zwakelemgabhi@gmail.com>
2025-09-29 15:22:21 +0200

    add env file

--- src/main/java/za/co/simplitate/springsecurity/EazyBankBackendApplication.java
+++ src/main/java/za/co/simplitate/springsecurity/EazyBankBackendApplication.java
@@ -2,8 +2,10 @@ package za.co.simplitate.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 @EnableWebSecurity
 @SpringBootApplication
 public class EazyBankBackendApplication {
